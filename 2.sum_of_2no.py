d50a5447b19731e6b907a62fec948dc0d8cb5eb2
iaaronb <bgaaron2022@gmail.com>
2026-05-20 20:35:34 -0700

    2.sum_of_2no.py

--- 2.sum_of_2no.py
+++ 2.sum_of_2no.py
@@ -1,4 +1,8 @@
+#taking the 1st input from the user
 X=int(input("Enter The First No.="))
+#taking the 2nd input from the user
 Y=int(input("Enter The Second No.="))
+#adding the 2 inputs and storing it in another variable
 Z=X+Y
+#printing the variable "Z"
 print(f"The Sum Of the No. ={Z}")
