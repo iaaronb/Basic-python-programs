e11d066973894fde9bfe89ccf0e36b117263e1a9
iaaronb <bgaaron2022@gmail.com>
2026-05-20 20:38:47 -0700

    4.largest_of_3no.py

--- 4.largest_of_3no.py
+++ 4.largest_of_3no.py
@@ -1,6 +1,9 @@
+#taking input and storing in a variable
 x=input("Enter the First No.for comaprison==")
+#taking input and storing in another variable
 y=input("Enter the Second No.for comaprison==")
+#implementing if statement to chechk whether which no. is greater
 if int(x>y):
 	print(f"{x} Is the largest no.")
-if int(y>x):
+elif int(y>x):
 	print(f"{y} Is the largest no.")
