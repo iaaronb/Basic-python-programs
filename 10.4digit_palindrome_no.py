e7aabb80029244764cd9b082446946abfd7f83b8
iaaronb <bgaaron2022@gmail.com>
2026-05-20 20:52:24 -0700

    10.4digit_palindrome_string.py

--- 10.4digit_palindrome_no.py
+++ 10.4digit_palindrome_no.py
@@ -1,5 +1,8 @@
+#taking the 1st input from the user
 x=input("Enter the no.")
+# setting the variable r with the reverse of x 
 y=x[::-1]
+#if statement to look whether it is a palindrome no.
 if x==y:
 	print("It is a palindrome number")
 else:	
