014fe2a3cdbcd2141d794f171213060605add017
iaaronb <bgaaron2022@gmail.com>
2026-05-20 20:43:10 -0700

    6.multiplication_table.py

--- 6.multiplication_table.py
+++ 6.multiplication_table.py
@@ -1,5 +1,8 @@
+#taking the 1st input from the user and storing a variable
 x=input("enter the no.==")
+#taking the 1st input from the user and storing in another variable
 y=input ("enter the no till multiplied")
+#adding a for loop for the multiplication table to be continue to the input from user
 for y in range(int(y)): 
 	print(f"{x} x {y+1}= {int(x)*int(y+1)}")
 	
