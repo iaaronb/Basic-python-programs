40a345ecfc790f3078554d13be8ef959e3c7a55d
iaaronb <bgaaron2022@gmail.com>
2026-05-20 20:47:01 -0700

    7.1toN_no_sum.py

--- 7.1toN_no_sum.py
+++ 7.1toN_no_sum.py
@@ -1,7 +1,10 @@
+#taking the 1st input from the user and storing a variable
 x=int(input("enter the no."))
 y=0
+#adding a for loop for the sum 1 to input from user
 for x in range(1,x+1):
 	print (x)
+	#adding the value with y until he loop stops
 	y+=x
 print(y)
 
