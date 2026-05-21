fe255943830e3f08b3654bf05ed578185babfc94
iaaronb <bgaaron2022@gmail.com>
2026-05-20 20:41:08 -0700

    5.largest_of_3no.py

--- 5.largest_of_3no.py
+++ 5.largest_of_3no.py
@@ -1,9 +1,15 @@
+#taking input and storing in a variable
 x=int(input("Enter A no.=="))
+#taking input and storing in a variable
 y=int(input("Enter A no.=="))
+#taking input and storing in a variable
 z=int(input("Enter a no.=="))
+#making if statements for the calculations
 if x>y>z or x>z>y:
 	print(f"{x} Is the largest no.")
+#making if statements for the calculations
 elif y>z>x or y>x>z:
 	print(f"{y} is the largest no.")
+#making if statements for the calculations
 elif z>y>x or z>x>y:
 	print(f"{z} is the largest no.")
