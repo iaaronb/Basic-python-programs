dae1c4d9b20a3d914c35621e13fa09fee9d53c03
iaaronb <bgaaron2022@gmail.com>
2026-05-20 20:49:29 -0700

    8.1toN_no_product.py

--- 8.1toN_no_product.py
+++ 8.1toN_no_product.py
@@ -1,6 +1,10 @@
+#taking the 1st input from the user and storing a variable
 x=int(input("enter the no."))
 y=1
+#adding a for loop for the product of  1 to input from user
+#setting the range from 1 to input +1
 for x in range(1,x+1):
 	print (x)
 	y=x*y   
+	
 print (y)
